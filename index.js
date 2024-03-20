490a074f47e05eb255efd62ece4e7352a6bb193c
bob <Mansalati18@gmail.com>
2024-03-20 20:26:43 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+  return customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = 'someone';
+
+function changeLeastFavoriteCustomer() {
+
+  leastFavoriteCustomer = 'someone else'; 
+}
+
+console.log(customerName);
+console.log(upperCaseCustomerName());
+console.log(bestCustomer);
+setBestCustomer();
+console.log(bestCustomer); 
+overwriteBestCustomer();
+console.log(bestCustomer);
+console.log(leastFavoriteCustomer);
+changeLeastFavoriteCustomer();
